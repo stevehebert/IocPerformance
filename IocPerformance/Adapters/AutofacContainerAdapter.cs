2206d6acfe8ff471e411233fc3c67770127a63da
Steve Hebert <steve.hebert@gmail.com>
2013-04-18 14:47:06 -0500

    added lazy container instantiation to one case

--- IocPerformance/Adapters/AutofacContainerAdapter.cs
+++ IocPerformance/Adapters/AutofacContainerAdapter.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Xml.Linq;
 using Autofac;
 using Autofac.Extras.DynamicProxy2;
@@ -9,8 +10,13 @@ namespace IocPerformance.Adapters
 {
     public sealed class AutofacContainerAdapter : IContainerAdapter
     {
-        private IContainer container;
+        private Lazy<IContainer> _container;
 
+        public AutofacContainerAdapter()
+        {
+            _container = new Lazy<IContainer>(Setup);
+        }
+       
         public string Version
         {
             get
@@ -24,48 +30,60 @@ namespace IocPerformance.Adapters
             }
         }
 
-        public bool SupportsInterception { get { return true; } }
-
-        public void Prepare()
+        public bool SupportsInterception
         {
+            get { return true; }
+        }
+
+        private IContainer Setup()
+        {
+
             var autofacContainerBuilder = new ContainerBuilder();
 
             autofacContainerBuilder.Register(c => new AutofacInterceptionLogger());
 
             autofacContainerBuilder.RegisterType<Singleton>()
-                    .As<ISingleton>()
-                    .SingleInstance();
+                                   .As<ISingleton>()
+                                   .SingleInstance();
 
             autofacContainerBuilder.RegisterType<Transient>()
-                    .As<ITransient>();
+                                   .As<ITransient>();
 
             autofacContainerBuilder.RegisterType<Combined>()
-                    .As<ICombined>();
+                                   .As<ICombined>();
 
             autofacContainerBuilder.RegisterType<Calculator>()
-                    .As<ICalculator>()
-                    .EnableInterfaceInterceptors();
+                                   .As<ICalculator>()
+                                   .EnableInterfaceInterceptors();
 
-            autofacContainerBuilder.RegisterType<First>().As<ISet>();
+            autofacContainerBuilder.RegisterType<First>()
+                                   .As<ISet>();
+                                   //.WithMetadata<SetMetadata>(new SetMetadata{Details="foo"});
+
             autofacContainerBuilder.RegisterType<Second>().As<ISet>();
+
+            return autofacContainerBuilder.Build();
+        }
+
+        public void Prepare()
+        {
             
-            this.container = autofacContainerBuilder.Build();
         }
 
         public T Resolve<T>() where T : class
         {
-            return this.container.Resolve<T>();
+            return _container.Value.Resolve<T>();
         }
 
         public T ResolveProxy<T>() where T : class
         {
-            return this.container.Resolve<T>();
+            return _container.Value.Resolve<T>();
         }
 
         public void Dispose()
         {
             // Allow the container and everything it references to be disposed.
-            this.container = null;
+            _container = null;
         }
     }
 }
